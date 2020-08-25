f7df5210f36f802d006e52158de9294496e8fd9f
Jonathan D'mello <jonathandmello@yahoo.co.in>
2020-08-25 12:03:28 +0530

    Device registration and deletion (collection creation needs to be checked)

--- routes/devices.js
+++ routes/devices.js
@@ -3,27 +3,28 @@ const router = express.Router();
 const socket = require('socket.io');
 const User = require('../models/users');
 const Device = require('../models/devices');
+const { db } = require('../models/devices');
 
 let values = [1,2,3,4,5,6,7];
 const io = socket(express().listen(9000));
 
-router.get('/', function(req, res, next) {
-    setInterval(()=> {
-        values.push(values[values.length-1] + 11)
-    },3000);
-    let valLength  =values.length;
-    res.render('devices', { title: 'devices' });
-    io.sockets.on('connection' , (socket) => {
-        io.emit("firstdata", {dataval:values})
-        setInterval(() => {
-            if(values.length != valLength){
-                io.emit("data", {dataval:values[valLength]}) 
-                valLength = values.length;
-                }
-        },100)
+// router.get('/', function(req, res, next) {
+//     setInterval(()=> {
+//         values.push(values[values.length-1] + 11)
+//     },3000);
+//     let valLength  =values.length;
+//     res.render('devices', { title: 'devices' });
+//     io.sockets.on('connection' , (socket) => {
+//         io.emit("firstdata", {dataval:values})
+//         setInterval(() => {
+//             if(values.length != valLength){
+//                 io.emit("data", {dataval:values[valLength]}) 
+//                 valLength = values.length;
+//                 }
+//         },100)
         
-    })
-  });
+//     })
+//   });
 
   router.route('/register')
   .get((req,res,next) => {
@@ -40,16 +41,27 @@ router.get('/', function(req, res, next) {
         if(device != null){
         User.updateOne({_id :req.user._id},{$push: { devices: device._id}})
         .then( user => {
-         res.statusCode = 200;
-        res.send(device);
+          db.createCollection(device._id.toString())
+          .then( () => {
+            res.statusCode = 200;
+            res.send(device);
+          })
         })
-        
         }
     },err => next(err))
-    .catch(err => next(err))
-    
+    .catch(err => next(err));
+  });
 
-  })
+    router.post('/delete/:idd',async (req,res,next) => {
+      res.send(req.params.idd);
+      if(req.isAuthenticated() == false)
+    return res.redirect('/users/login')
+    await Device.findByIdAndDelete(req.params.idd)
+    await User.updateOne({_id :req.user._id},{$pull: { devices: req.params.idd}});
+    res.send("Successful")
+    });
+
+
 
   
   
